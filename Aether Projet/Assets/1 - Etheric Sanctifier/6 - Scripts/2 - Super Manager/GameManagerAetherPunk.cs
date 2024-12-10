bc68ced3dbeca85e1a9d9055f362034e960bd0ee
JaaJ <Pqnk@users.noreply.github.com>
2024-12-10 09:52:54 +0100

    robhein

--- Aether Projet/Assets/1 - Etheric Sanctifier/6 - Scripts/2 - Super Manager/GameManagerAetherPunk.cs
+++ Aether Projet/Assets/1 - Etheric Sanctifier/6 - Scripts/2 - Super Manager/GameManagerAetherPunk.cs
@@ -7,7 +7,10 @@ public class GameManagerAetherPunk : MonoBehaviour
 {
     private void Awake()
     {
+    }
 
+    private void Start()
+    {
     }
 
     public void LaunchGameplay(LevelType levelType)
