ba35777fdd570ac03abea122b39a88c046522015
Robin <55254444+Team53FR@users.noreply.github.com>
2025-02-26 14:31:25 +0100

    Rangement bis

--- AFSFOR/Assets/1 - AFSFOR/5 - Scripts/1 - Super Manager/VibrationManager.cs
+++ AFSFOR/Assets/1 - AFSFOR/5 - Scripts/1 - Super Manager/VibrationManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VibrationManager : MonoBehaviour
 {
@@ -18,7 +19,13 @@ public class VibrationManager : MonoBehaviour
     public void InitializeVibrationManager()
     {
         GameObject g = levelManager.FindInScene(levelManager.GetCurrentLevelType(), "Camera_Player");
-        leftController = g.transform.GetChild(0).gameObject.GetComponent< ViveControllerVibration>();
-        rightController = g.transform.GetChild(1).gameObject.GetComponent<ViveControllerVibration>();
+                
+        GameObject cam = GameObject.Find("Camera_Player");
+
+        //leftController = g.transform.GetChild(0).gameObject.GetComponent<ViveControllerVibration>();
+        //rightController = g.transform.GetChild(1).gameObject.GetComponent<ViveControllerVibration>();
+
+        leftController = cam.transform.GetChild(0).gameObject.GetComponent<ViveControllerVibration>();
+        rightController = cam.transform.GetChild(1).gameObject.GetComponent<ViveControllerVibration>();
     }
 }
