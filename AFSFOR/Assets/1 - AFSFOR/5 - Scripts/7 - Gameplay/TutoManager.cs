71e4b91257ee8495b4f1e98aa257d876d018ccd8
Robin <55254444+Team53FR@users.noreply.github.com>
2025-03-05 15:53:25 +0100

    Add tuto ( a test )

---
+++ AFSFOR/Assets/1 - AFSFOR/5 - Scripts/7 - Gameplay/TutoManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class TutoManager : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] Transform[] spawnPoints;
+
+    Transform player;
+
+
+    void Start()
+    {
+        Player.instance.gameObject.GetComponent<Player_AFSFOR>().ToggleHeadLight(true);
+        player = Player.instance.transform;
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(5);
+
+        while (true)
+        {
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
